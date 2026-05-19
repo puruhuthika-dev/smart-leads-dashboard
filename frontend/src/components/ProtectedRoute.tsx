5c29d8a58bb1cdd7ce9c83116e0d04f27aa028d0
Puruhuthika <puruhuthika0801@gmail.com>
2026-05-19 17:18:49 +0530

    Fixed ProtectedRoute syntax

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,24 +1,17 @@
-import {
-  Navigate,
-} from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = ({
-  children,
-}: {
-  children: ): React.ReactElement;
-}) => {
-  const token =
-    localStorage.getItem(
-      "token"
-    );
+type Props = {
+  children: React.ReactNode;
+};
+
+const ProtectedRoute = ({ children }: Props) => {
+  const token = localStorage.getItem("token");
 
   if (!token) {
-    return (
-      <Navigate to="/" />
-    );
+    return <Navigate to="/" replace />;
   }
 
-  return children;
+  return <>{children}</>;
 };
 
 export default ProtectedRoute;
